c4071ce95a824a0d67b01ae369e01de41afcec61
zhangfan <brian@qvod.com>
2014-04-28 15:32:24 +0800

    add js

---
+++ javascripts/blog/StartApp.coffee
@@ -0,0 +1,9 @@
+define(['jquery', 'handlebars', 'turn'], ($, handlebars, turn)->
+	class StartApp
+
+		contructor: ()->
+			@initialize()
+
+		initialize: ()->
+			alert true
+)
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
 console.log('This would be the main JS file.');
+
+(function() {
+    var urlPath = location.pathname, version = "0.0.1";
+    require.config({
+        paths: {
+            jquery: "lib/jquery",
+            handlebars: "lib/handlebars",
+            turn: "lib/turn.min",
+            StartApp: "blog/StartApp"
+        },
+        shim: {
+            turn: {
+                deps: ['jquery']
+            }
+        }
+    })
+
+    require(['jquery', 'StartApp'], function($, StartApp) {
+        var init = new StartApp();
+    });
+}).call(this)
